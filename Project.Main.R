f0c4737363174f1e220350ff30f629c45b35df96
sabyasherpa <sabya.sherpa@gmail.com>
2025-03-28 12:28:31 +0100

    updates on variable tumorstage

--- Project.Main.R
+++ Project.Main.R
@@ -221,20 +221,6 @@ save.pdf(function(){
 plotMA(res)
 }, "MA plot")
 
-
-# Extract the top 10 upregulated and top 10 downregulated genes 
-top_upregulated_NSCLC <- head(deg_genes[order(-deg_genes$log2FoldChange), ], 10)
-top_downregulated_NSCLC <- head(deg_genes[order(-deg_genes$log2FoldChange), ], 10)
-
-top_upregulated_NSCLC_df <- as.data.frame(top_upregulated_NSCLC)
-top_downregulated_NSCLC_df <- as.data.frame(top_downregulated_NSCLC)
-
-# upregulated and downregulated genes
-upregulated_deg_genes <- subset(deg_genes, padj < 0.01 & log2FoldChange > 0)
-downregulated_deg_genes <- subset(deg_genes, padj < 0.01 & log2FoldChange < 0)
-
-
-
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 # DESeq2 for every variable
 
@@ -338,67 +324,7 @@ write.table(DEGs_control_vs_SC, file= "DEGs_control_vs_SC.tsv", sep = "\t", col.
 write.table(DEGs_control_vs_AC, file= "DEGs_control_vs_AC.tsv", sep = "\t", col.names = F)
 write.table(DEGs_control_vs_LC, file= "DEGs_control_vs_LC.tsv", sep = "\t", col.names = F)
 
-# upregulated and downregulated genes
-upregulated_control_vs_AC <- subset(DEGs_control_vs_AC, padj < 0.01 & log2FoldChange > 0)
-downregulated_control_vs_AC <- subset(DEGs_control_vs_AC, padj < 0.01 & log2FoldChange < 0)
-
-upregulated_control_vs_SC <- subset(DEGs_control_vs_SC, padj < 0.01 & log2FoldChange > 0)
-downregulated_control_vs_SC <- subset(DEGs_control_vs_SC, padj < 0.01 & log2FoldChange < 0)
-
-upregulated_control_vs_LC <- subset(DEGs_control_vs_LC, padj < 0.01 & log2FoldChange > 0)
-downregulated_control_vs_LC <- subset(DEGs_control_vs_LC, padj < 0.01 & log2FoldChange < 0)
-
-# DEseq analysis with adenocarcinoma as reference
-
-# Function to get the results for smoking
-DSQ2("Histology", "Control")
-
-metadata.subset$Histology <- as.factor(metadata.subset$Histology)
-
-# 2. Construct a DESeqDataSet object
-dds_histology <- DESeqDataSetFromMatrix(countData = raw_counts,
-                                        colData = metadata.subset,
-                                        design = ~ Histology)
-
-# 3. Quality control - Remove genes with low counts
-keep <- rowMeans(counts(dds_histology)) >=10
-dds_histology <- dds_histology[keep,]
-
-# 4. Set 'control' as the Reference
-dds_histology$Histology <- relevel(dds_histology$Histology, ref = "2")
-
-# 5. Run DESeq2
-dds_histology <- DESeq(dds_histology) 
-
-# 6. Extract DEGs for groups: current smokers, ex smokers, never smokers 
-res_AC_vs_SC <- results(dds_histology, contrast = c ("Histology", "2", "1"))
-res_AC_vs_LC <- results(dds_histology, contrast = c ("Histology", "2", "3"))
-
-# 7. Extract DEGs within each comparison individually
-DEGs_AC_vs_SC <- res_control_vs_SC[which(res_control_vs_SC$padj < 0.01 & abs(res$log2FoldChange) > 1), ]
-DEGs_AC_vs_LC <- res_control_vs_LC[which(res_control_vs_LC$padj < 0.01 & abs(res$log2FoldChange) > 1), ]
-
-DEGs_AC_vs_SC <- res_control_vs_SC[which(res_control_vs_SC$padj < 0.01 & abs(res_control_vs_SC$log2FoldChange) > 1), ]
-DEGs_AC_vs_LC <- res_control_vs_LC[which(res_control_vs_LC$padj < 0.01 & abs(res_control_vs_LC$log2FoldChange) > 1), ]
-
-DEGs_AC_vs_SC <- res_control_vs_SC[which(res_control_vs_SC$padj < 0.01 & abs(res_control_vs_SC$log2FoldChange) > 1), ]
-DEGs_AC_vs_LC <- res_control_vs_LC[which(res_control_vs_LC$padj < 0.01 & abs(res_control_vs_LC$log2FoldChange) > 1), ]
-
-# write a table for DEGs
-write.table(DEGs_AC_vs_SC, file= "DEGs_AC_vs_SC.tsv", sep = "\t", col.names = F)
-write.table(DEGs_AC_vs_LC, file= "DEGs_AC_vs_LC.tsv", sep = "\t", col.names = F)
-
-# make a plot
-plot_volcano(res_AC_vs_LC, "Volcano plot AC vs LC")
-plot_volcano(res_AC_vs_SC, "Volcano plot AC vs SC")
-
-# upregulated and downregulated genes
-upregulated_AC_vs_LC <- subset(DEGs_AC_vs_LC, padj < 0.01 & log2FoldChange > 0)
-downregulated_AC_vs_SC <- subset(DEGs_AC_vs_LC, padj < 0.01 & log2FoldChange < 0)
-
-upregulated_AC_vs_LC <- subset(DEGs_AC_vs_LC, padj < 0.01 & log2FoldChange > 0)
-downregulated_AC_vs_SC <- subset(DEGs_AC_vs_LC, padj < 0.01 & log2FoldChange < 0)
-
+# 9. Making a plot 
 
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=
 # DESeq2 for sex
@@ -530,153 +456,163 @@ head(resOrdered)
 
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=
 # DESeq 2 for tumor stage - Sabya
-# 1. change variables from chracters to factors for tumor
-metadata.subset$Tumor_stage <- as.factor(metadata.subset$Tumor_stage)
+# Step 1: Convert to character (just to avoid factor issues)
+metadata.subset$Tumor_stage <- as.character(metadata.subset$Tumor_stage)
 
-# 2. construct deseq set for Tumor_stage
+# Step 2: Recode sub-stages to single-digit stages
+metadata.subset$Tumor_stage <- dplyr::recode(metadata.subset$Tumor_stage,
+                                             "1" = "1", "2" = "1",
+                                             "3" = "2", "4" = "2",
+                                             "5" = "3", "6" = "3",
+                                             "7"  = "4",
+                                             "Control" = "Control"
+)
+
+# Step 3: Turn into factor, and set order if needed
+metadata.subset$Tumor_stage <- factor(metadata.subset$Tumor_stage,
+                                      levels = c("Control", "1", "2", "3", "4"))
+
+# Step 4: Check it worked
+table(metadata.subset$Tumor_stage)
+
+# Step 5. construct deseq set for Tumor_stage
 dds_Tumorstage <- DESeqDataSetFromMatrix(countData = raw_counts,
                                          colData = metadata.subset,
                                          design = ~ Tumor_stage)
-# Quality control
+# Step 6. Quality control
 # Remove genes with low counts
 keep <- rowMeans(counts(dds_Tumorstage)) >= 10
 dds_Tumorstage <- dds_Tumorstage[keep, ]
 
-# 4. Set 'control' as the Reference
+# Step 7. Set 'control' as the Reference
 dds_Tumorstage$Tumor_stage <- relevel(dds_Tumorstage$Tumor_stage, ref = "Control")
 
 
-dds$Tumor_stage <- recode_factor(as.character(dds$Tumor_stage),
-                                 "1" = "Stage 1",
-                                 "2" = "Stage 1",
-                                 "3" = "Stage 2",
-                                 "4" = "Stage 2",
-                                 "5" = "Stage 3",
-                                 "6" = "Stage 3",
-                                 "7" = "Stage 4")
-
-dds$Tumor_stage <- factor(dds$Tumor_stage,
-                          levels = c("Control", "Stage 1", "Stage 2", "Stage 3", "Stage 4"))
-
-# 5. Run DESeq2
+# Step 8. Run DESeq2
 dds_Tumorstage <- DESeq(dds_Tumorstage) 
 
-# 6. Extract DEGs for groups: current smokers, ex smokers, never smokers 
+# Step 9. Extract DEGs for all stages
 res_control_vs_stage1 <- results(dds_Tumorstage, contrast = c ("Tumor_stage", "Control", "1"))
 res_control_vs_stage2 <- results(dds_Tumorstage, contrast = c ("Tumor_stage", "Control", "2"))
 res_control_vs_stage3 <- results(dds_Tumorstage, contrast = c ("Tumor_stage", "Control", "3"))
-res_control_vs_stage4 <- results(dds_Tumorstage, contrast = c ("Tumor_stage", "Control", "4"))
 
-tumor_res <- c(res_control_vs_stage1, res_control_vs_stage2, res_control_vs_stage3, res_control_vs_stage4)
 
-# 7. Extract DEGs within each comparison individually
+# Step 10. Extract DEGs within each comparison individually
 DEGs_control_vs_stage1 <- res_control_vs_stage1[which(res_control_vs_stage1$padj < 0.01 & abs(res_control_vs_stage1$log2FoldChange) > 1), ]
 DEGs_control_vs_stage2 <- res_control_vs_stage2[which(res_control_vs_stage2$padj < 0.01 & abs(res_control_vs_stage2$log2FoldChange) > 1), ]
 DEGs_control_vs_stage3 <- res_control_vs_stage3[which(res_control_vs_stage3$padj < 0.01 & abs(res_control_vs_stage3$log2FoldChange) > 1), ]
-DEGs_control_vs_stage4 <- res_control_vs_stage4[which(res_control_vs_stage4$padj < 0.01 & abs(res_control_vs_stage4$log2FoldChange) > 1), ]
-
-# 8 Check how many significant DEGs were found
-nrow(DEGs_control_vs_stage4)
 
 
-# 9making plots
+# Step 11. Check how many significant DEGs were found
+nrow(DEGs_control_vs_stage1)
+nrow(DEGs_control_vs_stage2)
+nrow(DEGs_control_vs_stage3)
 
-# Convert results to a dataframe
-res_tumorstage <- as.data.frame(res_tumorstage)
+
+# step 12. Save DEG list
+write.table(rownames(DEGs_control_vs_stage1), "DEGs_control_vs_stage1.csv", sep = "\t", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(rownames(DEGs_control_vs_stage2), "DEGs_control_vs_stage2.csv", sep = "\t", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(rownames(DEGs_control_vs_stage3), "DEGs_control_vs_stage3.csv", sep = "\t", quote = FALSE, row.names = FALSE, col.names = FALSE)
+
+#making plots
+plot_volcano(res_control_vs_stage1, "control vs Stage 1")
+plot_volcano(res_control_vs_stage2, "control vs Stage 2")
+plot_volcano(res_control_vs_stage3, "control vs Stage 3")
+
+#comparison within tumorstages only 
+
+# Subset metadata and counts to tumor stages only (1, 2, 3)
+metadata_tumorOnly <- metadata.subset[metadata.subset$Tumor_stage %in% c("1", "2", "3"), ]
+counts_tumorOnly <- raw_counts[, rownames(metadata_tumorOnly)]
+
+# Make sure the stage is a factor
+metadata_tumorOnly$Tumor_stage <- factor(metadata_tumorOnly$Tumor_stage,
+                                         levels = c("1", "2", "3"))
+
+# Create DESeqDataSet
+dds_tumorOnly <- DESeqDataSetFromMatrix(
+  countData = counts_tumorOnly,
+  colData = metadata_tumorOnly,
+  design = ~ Tumor_stage
+)
+
+# Filter low-expressed genes
+dds_tumorOnly <- dds_tumorOnly[rowMeans(counts(dds_tumorOnly)) >= 10, ]
+
+# Relevel to Stage 1
+dds_stage1ref <- dds_tumorOnly
+dds_stage1ref$Tumor_stage <- relevel(dds_stage1ref$Tumor_stage, ref = "1")
+dds_stage1ref <- DESeq(dds_stage1ref)
+
+# Extract results
+res_2_vs_1 <- results(dds_stage1ref, contrast = c("Tumor_stage", "2", "1"))
+res_3_vs_1 <- results(dds_stage1ref, contrast = c("Tumor_stage", "3", "1"))
+
+# Filter DEGs
+DEGs_2_vs_1 <- res_2_vs_1[which(res_2_vs_1$padj < 0.01 & abs(res_2_vs_1$log2FoldChange) > 1), ]
+DEGs_3_vs_1 <- res_3_vs_1[which(res_3_vs_1$padj < 0.01 & abs(res_3_vs_1$log2FoldChange) > 1), ]
+
+#Check how many significant DEGs were found
+nrow(DEGs_2_vs_1)
+nrow(DEGs_3_vs_1)
 
 
+# Save DEG lists
+write.table(rownames(DEGs_2_vs_1), "DEGs_Stage2_vs_Stage1.csv", sep = "\t", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(rownames(DEGs_3_vs_1), "DEGs_Stage3_vs_Stage1.csv", sep = "\t", quote = FALSE, row.names = FALSE, col.names = FALSE)
 
-# Create a column for significance
-res_control_vs_stage1$significance <- ifelse(res_$padj < 0.01 & abs(res_df$log2FoldChange) > 1,
-                                             ifelse(res_df$log2FoldChange > 1, "Upregulated", "Downregulated"),
-                                             "Not Significant")
-print(res_df$significance)
+# Relevel to Stage 2
+dds_stage2ref <- dds_tumorOnly
+dds_stage2ref$Tumor_stage <- relevel(dds_stage2ref$Tumor_stage, ref = "2")
+dds_stage2ref <- DESeq(dds_stage2ref)
 
-# Plot Volcano Plot
-save.pdf(function(){
-  ggplot(res_df, aes(x = log2FoldChange, y = -log10(padj), color = significance)) +
-    geom_point(alpha = 0.6) +
-    geom_vline(xintercept = c(-1, 1), linetype = "dotted", color = "black") +  # Vertical cutoff lines
-    geom_hline(yintercept = -log10(0.01), linetype = "dotted", color = "black") +  # Horizontal cutoff line
-    scale_color_manual(values = c("Upregulated" = "red", "Downregulated" = "blue", "Not Significant" = "grey")) +
-    theme_minimal() +
-    labs(title = "Volcano Plot of DEGs in NSCLC Tissue vs. Normal Tissue", 
-         x = "Log2 Fold Change", 
-         y = "-Log10 Adjusted P-Value") +
-    theme(legend.title = element_blank())
-}, "Volcano Plot")
+# Extract results
+res_3_vs_2 <- results(dds_stage2ref, contrast = c("Tumor_stage", "3", "2"))
 
-plot_volcano(res_control_vs_stage1, "tumorstage 1 vs control")
-plot_volcano(res_control_vs_stage2, "tumorstage 2 vs control")
-plot_volcano(res_control_vs_stage3, "tumorstage 3 vs control")
-plot_volcano(res_control_vs_stage4, "tumorstage 4 vs control")
+# Filter DEGs
+DEGs_3_vs_2 <- res_3_vs_2[which(res_3_vs_2$padj < 0.01 & abs(res_3_vs_2$log2FoldChange) > 1), ]
 
+# Check how many significant DEGs were found
+nrow(DEGs_3_vs_2)
+
+# Save DEG list
+write.table(rownames(DEGs_3_vs_2), "DEGs_Stage3_vs_Stage2.csv", sep = "\t", quote = FALSE, row.names = FALSE, col.names = FALSE)
+
+#making plots
+plot_volcano(res_2_vs_1, "Stage 2 vs Stage 1")
+plot_volcano(res_3_vs_1, "Stage 3 vs Stage 1")
+plot_volcano(res_3_vs_2, "Stage 3 vs Stage 2")
 
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=
-# Step 3. GO, heatmap of 41 DEGs in NSCLC samples vs non malignant samples, sig. associated with lungcancer
+  # Step 3. GO
 
-# function Bart
+<<<<<<< HEAD
 heatmap_plot(raw_counts, "sex", DEGs_male_vs_female)
 
-# Insert MOET output, ie the genes from the DEGs associated with lungdisease 
+
+# heatmap for 41 DEGs in NSCLC vs non-malignant, significantly associated with lung cancer 
+
+# insert MOET output, ie the genes from the DEGs associated with lungdisease 
 moetgenessource <- read.table("MOET genes lung cancer source ensg.txt", header = FALSE, stringsAsFactors = FALSE)
 colnames(moetgenessource) <- c("Gene_Symbol") 
 
-# Check overlap with expression data 
+# check overlap with expression data 
 overlap_moetgenessource <- moetgenessource$Gene_Symbol[moetgenessource$Gene_Symbol %in% rownames(raw_counts)]
 
 # Subset the data for MOET genes
 expression_moetgenessource <- raw_counts[rownames(raw_counts) %in% overlap_moetgenessource, ]
 
-# Normalize it
+# normalize it
 log_overlap_moetgenessource <- log2(as.matrix(expression_moetgenessource) + 1)
 
-# Split Heat Map into 2, Healthy & Non-Healthy
-# Separate Tumor (“T”) and Normal (“N”) Samples
-# Select columns ending with "T"
-tumor_samples <- log_overlap_moetgenessource[, grep("T$", colnames(log_overlap_moetgenessource))]
-
-# Plot heatmap for tumor samples
-pheatmap(tumor_samples, 
-         cluster_rows = TRUE, 
-         cluster_cols = TRUE, 
-         scale = "row",
-         show_rownames = TRUE, 
-         show_colnames = TRUE, 
-         main = "Heatmap of MOET Genes in Tumor Samples")
-
-# Select columns ending with "N"
-normal_samples <- log_overlap_moetgenessource[, grep("N$", colnames(log_overlap_moetgenessource))]
-
-# Plot heatmap for normal samples
-pheatmap(normal_samples, 
-         cluster_rows = TRUE, 
-         cluster_cols = TRUE, 
-         scale = "row",
-         show_rownames = TRUE, 
-         show_colnames = TRUE, 
-         main = "Heatmap of MOET Genes in Normal Samples")
-
-
-### Put them All in One figure 
-# Order columns: Normal (N) first, Tumor (T) second
-ordered_columns <- c(grep("N$", colnames(log_overlap_moetgenessource), value = TRUE), 
-                     grep("T$", colnames(log_overlap_moetgenessource), value = TRUE))
-log_overlap_moetgenessource <- log_overlap_moetgenessource[, ordered_columns]
-
-# Create a column annotation to distinguish Tumor vs. Normal
-sample_types <- ifelse(grepl("T$", colnames(log_overlap_moetgenessource)), "Tumor", "Normal")
-annotation_col <- data.frame(Type = factor(sample_types, levels = c("Normal", "Tumor")))
-rownames(annotation_col) <- colnames(log_overlap_moetgenessource)
-
-# Plot the heatmap
+# heatmap
 pheatmap(log_overlap_moetgenessource, 
          cluster_rows = TRUE, 
          cluster_cols = TRUE, 
          scale = "row",
          show_rownames = TRUE, 
-         show_colnames = TRUE, 
-         annotation_col = annotation_col,  # Highlight Tumor vs. Normal
-         gaps_col = length(grep("N$", colnames(log_overlap_moetgenessource))), # Add gap between groups
-         main = "Heatmap of MOET Genes (Normal vs. Tumor)")
+         show_colnames = FALSE, 
+         main = "Heatmap of MOET Genes in Lung Cancer")
 
+=======
+>>>>>>> ae9d544a686b68b3d8e54a5edda75a28d88c1d04
